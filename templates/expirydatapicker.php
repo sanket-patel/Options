62b9306d611b27face80df5d4842d11b13b72e1d
sanket-patel <sanket.patel@outlook.com>
2015-04-10 21:25:00 -0400

    moved to datepicker element to its own file

---
+++ templates/expirydatapicker.php
@@ -0,0 +1,13 @@
+<div data-toggle="tooltip" title="Enter a date as mm/dd/yyyy or pick one from the date picker">
+	<div class="panel panel-default">
+		<div class="panel-heading"><span class="label label-default label-as-badge">2</span>&nbsp&nbspSelect option expiry</div>
+			<div class="panel-body">
+				<div class="input-append date">
+					<!--- type-"date" is currently not supported in firefox and ie --->
+					<!--- input type="date" id="expiry" name="expiry" /> --->
+					<input class="datepicker" id="expiry" name="expiry" />
+					<span class="add-on"><i class="icon-th"></i></span>
+				</div> <!--- END date input --->
+			</div> <!--- END panel-body --->
+	</div> <!--- END panel for expiry--->
+</div>
