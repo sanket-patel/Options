274077bb0d08e2547c7a81446a9b7f638a1c2937
sanket-patel <sanket.patel@outlook.com>
2015-04-10 21:29:02 -0400

    replaced datepicker element with include of expirydatepicker.php

--- dh.php
+++ dh.php
@@ -56,19 +56,10 @@
 				include('templates/etfdropdown.php');
 			?>
 			
-			<div data-toggle="tooltip" title="Enter a date as mm/dd/yyyy or pick one from the date picker">
-				<div class="panel panel-default">
-					<div class="panel-heading"><span class="label label-default label-as-badge">2</span>&nbsp&nbspSelect option expiry</div>
-						<div class="panel-body">
-							<div class="input-append date">
-								<!--- type-"date" is currently not supported in firefox and ie --->
-								<!--- input type="date" id="expiry" name="expiry" /> --->
-								<input class="datepicker" id="expiry" name="expiry" placeholder="12/31/2014" />
-								<span class="add-on"><i class="icon-th"></i></span>
-							</div> <!--- END date input --->
-						</div> <!--- END panel-body --->
-				</div> <!--- END panel for expiry--->
-			</div>
+			<?php
+				#import html for date picker for expiry
+				include('templates/expirydatepicker.php');
+			?>
 			
 			<div data-toggle="tooltip" title="Enter an implied volatility, for example 20%">
 				<div class="panel panel-default">
--- iv.php
+++ iv.php
@@ -54,19 +54,10 @@
 				include('templates/etfdropdown.php');
 			?>
 			
-			<div data-toggle="tooltip" title="Enter a date as mm/dd/yyyy or pick one from the date picker">
-				<div class="panel panel-default">
-					<div class="panel-heading"><span class="label label-default label-as-badge">2</span>&nbsp&nbspSelect option expiry</div>
-						<div class="panel-body">
-							<div class="input-append date">
-								<!--- type-"date" is currently not supported in firefox and ie --->
-								<!--- input type="date" id="expiry" name="expiry" /> --->
-								<input class="datepicker" id="expiry" name="expiry" />
-								<span class="add-on"><i class="icon-th"></i></span>
-							</div> <!--- END date input --->
-						</div> <!--- END panel-body --->
-				</div> <!--- END panel for expiry--->
-			</div>
+			<?php
+				#import html for date picker for expiry
+				include('templates/expirydatepicker.php');
+			?>
 			
 			<div data-toggle="tooltip" title="Enter an option premium, for example 20">
 				<div class="panel panel-default">
